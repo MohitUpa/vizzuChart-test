b4d8507abb12c7e3cb31ce3adbcc7b058b62fda9
Mohit <mohitu531@gmail.com>
2022-07-27 14:38:39 +0530

    vizzu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+// import '../assets/vizzu.js';
+declare const Vizzu:any; 
+declare const data:any; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  ngOnInit(): void {
+      //  test1.test()
+  }
   title = 'vizzuChart';
+
+  ngAfterViewInit() {
+
+
+    let chart = new Vizzu('vizzuCanvas');
+
+    chart.animate({
+      data: data,
+      config: {
+        channels: {
+          color: { set: ['Joy factors'] },
+          label: { set: ['Country_code'] },
+          size: { set: ['Country_code', 'Value 2 (+)'] }
+        },
+        title: 'Stacked Bubble Chart',
+        geometry: 'circle'
+      }
+    });
+
+
+    chart.animate({
+      config: {
+        channels: {
+          x: { set: ['Country_code', 'Value 2 (+)'] },
+          y: { set: ['Joy factors'] },
+          label: { set: null },
+          /* The stacking is eliminated when we remove
+          the extra dimension from the size channel. */
+          size: { set: null }
+        },
+        title: 'Bar Chart',
+        geometry: 'rectangle',
+        orientation: 'vertical'
+      }
+    });
+
+
+    chart.animate({
+      config: {
+        channels: {
+          x: { detach: ['Country_code'] },
+          label: { set: ['Value 2 (+)'] }
+        }
+      }
+    });
+
+
+
+
+    // let data = {
+    //   series: [
+    //     { name: 'Foo', values: ['Alice', 'Bob', 'Ted'] },
+    //     { name: 'Bar', values: [15, 32, 12] },
+    //     { name: 'Baz', values: [5, 3, 2] }
+    //   ]
+    // };
+
+    // console.log(data);
+
+        // let chart = new Vizzu('myVizzu', { data });
+
+    // chart.animate({
+    //   x: 'Foo',
+    //   y: 'Bar'
+    // });
+
+  }
+
+
+
 }
---
+++ src/assets/vizzu.js
@@ -0,0 +1,49 @@
+// import Vizzu from 'https://cdn.jsdelivr.net/npm/vizzu@latest/dist/vizzu.min.js';
+
+// import { data } from 'https://lib.vizzuhq.com/test/integration/test_data/chart_types_eu.js';
+
+// console.log('TestingFunction')
+// let chart = new Vizzu('vizzuCanvas');
+
+// chart.animate({
+//     data: data,
+//     config: {
+//         channels: {
+//             color: { set: ['Joy factors'] },
+//             label: { set: ['Country_code'] },
+//             size: { set: ['Country_code', 'Value 2 (+)'] }
+//         },
+//         title: 'Stacked Bubble Chart',
+//         geometry: 'circle'
+//     }
+// });
+
+// chart.animate({
+//     config: {
+//         channels: {
+//             x: { set: ['Country_code', 'Value 2 (+)'] },
+//             y: { set: ['Joy factors'] },
+//             label: { set: null },
+//             /* The stacking is eliminated when we remove
+//             the extra dimension from the size channel. */
+//             size: { set: null }
+//         },
+//         title: 'Bar Chart',
+//         geometry: 'rectangle',
+//         orientation: 'vertical'
+//     }
+// });
+
+// chart.animate({
+//     config: {
+//         channels: {
+//             x: { detach: ['Country_code'] },
+//             label: { set: ['Value 2 (+)'] }
+//         }
+//     }
+// });
+
+// export function test() {
+//     console.log("test my js");
+// }
+
